4be13e35ff11d429c9c1a48cbe7b347c6b34d061
Valkiria <valkiria0000@bk.ru>
2022-01-28 00:37:52 +0300

    profile-block

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function none_menu(id) {
+   if($('#'+id).css('display') =='none') {
+      $ ('#'+id).css('display',""); }
+ 
+   else {
+      $('#'+id).css('display','none');} 
+      return false;
+   }
+
+   
+  
