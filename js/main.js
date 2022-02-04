de2d8c741f69a61402793b9f84c607a1d8a9038b
Valkiria <valkiria0000@bk.ru>
2022-02-04 16:51:06 +0300

    выпадающие блоки

--- js/main.js
+++ js/main.js
@@ -10,12 +10,21 @@ function none_menu(id) {
    
   
    function rewers(id) {
-      if ($ ('#'+id).css('display') == "none") {
-         $ ('#'+id).css('display', '');
+       console.log($ ('#'+id).css('display'))
+      if ($ ('#'+id).css('display') == 'none') {
+         $ ('#'+id).css('transform', 'rotate(0deg)'); 
          }
-    
+   
       else {
          $ ('#'+id).css('transform', 'rotate(180deg)');}
          return false;
       }
-   
+
+     
+ /*
+      function hideshow() {
+         document.getElementById("slider").style="display:none";
+         document.getElementById("other").style="display:block";
+      }
+
+     */
